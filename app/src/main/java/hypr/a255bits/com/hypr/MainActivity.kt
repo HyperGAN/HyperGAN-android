7837a48b3dbc15119c6d68977b9d9151a8f51a41
ted <tedhoryczuniv@outlook.com>
2017-07-17 13:03:02 -0400

    Button click test

--- app/src/main/java/hypr/a255bits/com/hypr/MainActivity.kt
+++ app/src/main/java/hypr/a255bits/com/hypr/MainActivity.kt
@@ -2,12 +2,21 @@ package hypr.a255bits.com.hypr
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainMvp.view{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    fun chooseImageClick(view: View){
+
+    }
+
+    override fun displayGallery() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
