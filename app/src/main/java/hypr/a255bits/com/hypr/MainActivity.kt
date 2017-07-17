b0abdd58817201ff98995ec85909309d77dd6c29
ted <tedhoryczuniv@outlook.com>
2017-07-17 13:29:57 -0400

    Choose Image Click opens gallery

--- app/src/main/java/hypr/a255bits/com/hypr/MainActivity.kt
+++ app/src/main/java/hypr/a255bits/com/hypr/MainActivity.kt
@@ -1,11 +1,15 @@
 package hypr.a255bits.com.hypr
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 
 class MainActivity : AppCompatActivity(), MainMvp.view{
     val presenter by lazy{MainPresenter(applicationContext, this)}
+    val galleryFileLocation: Uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+    private val  RESULT_GET_IMAGE: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +22,9 @@ class MainActivity : AppCompatActivity(), MainMvp.view{
 
     }
 
+
     override fun displayGallery() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val intent = Intent(Intent.ACTION_PICK, galleryFileLocation)
+        startActivityForResult(intent, RESULT_GET_IMAGE)
     }
 }
