2cef62a422709dd9765634e8b79c21e6ef77760f
ted <tedhoryczuniv@outlook.com>
2017-07-17 12:55:01 -0400

    "Choose Image" button, and bootstrap theme

--- app/src/main/java/hypr/a255bits/com/hypr/MainActivity.kt
+++ app/src/main/java/hypr/a255bits/com/hypr/MainActivity.kt
@@ -1,12 +1,13 @@
 package hypr.a255bits.com.hypr
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
