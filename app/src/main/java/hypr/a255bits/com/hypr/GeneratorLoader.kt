0374b7c72942fe463f2798a634feedcd24f62ff7
Martyn Garcia <martyn@255bits.com>
2017-07-18 22:00:26 -0600

    [feature] pseudo code for tensorflow, once I figure out this package issue

--- app/src/main/java/hypr/a255bits/com/hypr/GeneratorLoader.kt
+++ app/src/main/java/hypr/a255bits/com/hypr/GeneratorLoader.kt
@@ -1,16 +1,21 @@
 package hypr.a255bits.com.hypr
-
+import org.tensorflow.contrib.android.TensorflowInferenceInterface
 
 class Generator {}
 
 class GeneratorLoader {
     fun load(assetPath:String):Generator {
         print("Loading" + assetPath)
+        //model : String  = ...
+        //manager:AssetManager=...
+        //inference:TensorflowInferenceInterface = TensorflowInferenceInterface(manager, string)
         return Generator()
     }
 
     fun sample(generator:Generator) {
         print("Sampling " + generator)
+        //inference.run(..)
+        //inference.getTensor(generator.outputNode)
     }
 
 }
