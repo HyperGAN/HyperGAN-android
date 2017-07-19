912d9a6937db1c40528a45868f6cc75d29019e02
ted <tedhoryczuniv@outlook.com>
2017-07-19 13:49:47 -0400

    Base url to constant

--- app/src/main/java/hypr/a255bits/com/hypr/Network/ModelApi.kt
+++ app/src/main/java/hypr/a255bits/com/hypr/Network/ModelApi.kt
@@ -1,13 +1,16 @@
 package hypr.a255bits.com.hypr.Network
 
+import android.content.Context
 import hypr.a255bits.com.hypr.Generator
+import hypr.a255bits.com.hypr.R
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class ModelApi: ModelService{
+class ModelApi(): ModelService{
+    private val BASE_URL = "https://gist.githubusercontent.com"
     val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://gist.githubusercontent.com")
+            .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     val service: ModelService? = retrofit.create(ModelService::class.java)
