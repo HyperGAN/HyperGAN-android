d96aaaa72c31ff6fa31fd3b8b398a1ae96750d18
Martyn Garcia <martyn@255bits.com>
2017-07-18 22:45:07 -0600

    [refactor] remove generator requirement

--- app/src/main/java/hypr/a255bits/com/hypr/GeneratorLoader.kt
+++ app/src/main/java/hypr/a255bits/com/hypr/GeneratorLoader.kt
@@ -2,16 +2,10 @@ package hypr.a255bits.com.hypr
 import android.content.res.AssetManager
 import org.tensorflow.contrib.android.TensorFlowInferenceInterface
 
-class Generator {}
 
-class GeneratorLoader {
-    fun load(assetPath:String, assetManager:AssetManager):Generator {
-        print("Loading" + assetPath)
-        val model:String = "model.pbgraph"
-        val inference = TensorFlowInferenceInterface(assetManager, model)
-        //TODO store
-        return Generator()
-    }
+class GeneratorLoader(assetPath:String, assetManager:AssetManager) {
+
+    var inference:TensorFlowInferenceInterface = TensorFlowInferenceInterface(assetManager, assetPath)
 
     fun sample(generator:Generator) {
         print("Sampling " + generator)
