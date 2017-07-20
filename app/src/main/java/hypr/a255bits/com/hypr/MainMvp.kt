e4204245d11629840983a78add0562e85c3ddb21
ted <tedhoryczuniv@outlook.com>
2017-07-20 09:27:23 -0400

    Remove refreshLayout in interface

--- app/src/main/java/hypr/a255bits/com/hypr/MainMvp.kt
+++ app/src/main/java/hypr/a255bits/com/hypr/MainMvp.kt
@@ -8,7 +8,6 @@ interface MainMvp {
     interface view{
         fun displayGallery()
         fun displayFocusedImage(imageFromGallery: Bitmap)
-        fun refreshLayout()
 
     }
     interface presenter{
