ee4a01d491d2eb308881c4befb5037d795f86746
ted <tedhoryczuniv@outlook.com>
2017-07-24 15:07:47 -0400

    Added camera library and skeleton

---
+++ app/src/main/java/hypr/a255bits/com/hypr/CameraFragment/CameraFragment.kt
@@ -0,0 +1,53 @@
+package hypr.a255bits.com.hypr.CameraFragment
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import hypr.a255bits.com.hypr.R
+
+class CameraFragment : Fragment() {
+
+    private var mParam1: String? = null
+    private var mParam2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (arguments != null) {
+            mParam1 = arguments.getString(ARG_PARAM1)
+            mParam2 = arguments.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_camera, container, false)
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+
+    companion object {
+        private val ARG_PARAM1 = "param1"
+        private val ARG_PARAM2 = "param2"
+
+        fun newInstance(param1: String, param2: String): CameraFragment {
+            val fragment = CameraFragment()
+            val args = Bundle()
+            args.putString(ARG_PARAM1, param1)
+            args.putString(ARG_PARAM2, param2)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
