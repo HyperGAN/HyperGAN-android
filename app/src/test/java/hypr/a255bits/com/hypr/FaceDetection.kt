87c346c232cd006e5fe809b8678da1d65fc8a410
ted <tedhoryczuniv@outlook.com>
2017-07-24 13:35:30 -0400

    Choose image matching color to rest of app

--- app/src/test/java/hypr/a255bits/com/hypr/FaceDetection.kt
+++ app/src/test/java/hypr/a255bits/com/hypr/FaceDetection.kt
@@ -8,14 +8,17 @@ import com.nhaarman.mockito_kotlin.verify
 import hypr.a255bits.com.hypr.Main.MainInteractor
 import hypr.a255bits.com.hypr.Main.MainMvp
 import hypr.a255bits.com.hypr.Main.MainPresenter
+import hypr.a255bits.com.hypr.ModelFragmnt.ModelFragmentMVP
+import hypr.a255bits.com.hypr.ModelFragmnt.ModelFragmentPresenter
+import hypr.a255bits.com.hypr.ModelFragmnt.ModelInteractor
 import org.junit.Test
 
 class FaceDetection{
     val context: Context = mock()
-    val view: MainMvp.view = mock()
-    val interactor: MainInteractor = mock()
+    val view: ModelFragmentMVP.view = mock()
+    val interactor: ModelInteractor = mock()
 
-    val presenter: MainPresenter = MainPresenter(view, interactor, context)
+    val presenter: ModelFragmentPresenter = ModelFragmentPresenter(view, interactor, context)
 
     val testBitmap: Bitmap = any()
 
