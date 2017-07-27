030b0e5daf40356047c17f64b131a3896f23abe6
ted <tedhoryczuniv@outlook.com>
2017-07-27 10:21:44 -0400

    Cleanup

--- app/src/main/java/hypr/a255bits/com/hypr/GeneratorLoader.kt
+++ app/src/main/java/hypr/a255bits/com/hypr/GeneratorLoader.kt
@@ -2,47 +2,55 @@ package hypr.a255bits.com.hypr
 
 import android.content.res.AssetManager
 import android.graphics.Bitmap
-import android.util.Log
 import org.tensorflow.contrib.android.TensorFlowInferenceInterface
 
-class GeneratorLoader() {
-    public var inference:TensorFlowInferenceInterface? = null
+class GeneratorLoader {
+    lateinit var inference: TensorFlowInferenceInterface
 
     var channels = 3
     var width = 128
     var height = 128
-    public var bitmap: Bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
 
-    public var raw:FloatArray = FloatArray(width*height*channels)
+    var raw: FloatArray = FloatArray(width * height * channels)
 
-    fun load(assets:AssetManager) {
+    fun load(assets: AssetManager) {
         System.loadLibrary("tensorflow_inference")
 
-        var pbfile:String = "file:///android_asset/generators/optimized_weight_conv.pb"
+        val pbfile: String = "file:///android_asset/generators/optimized_weight_conv.pb"
         this.inference = TensorFlowInferenceInterface(assets, pbfile)
 
 
     }
-    fun sample():Bitmap {
+
+    fun sample(): Bitmap {
         print("Sampling ")
 
-        this.inference!!.run(arrayOf<String>("Tanh_1"))
-
-        val intValues = IntArray(width * height)
-
+        this.inference.run(arrayOf("Tanh_1"))
         //inference.readNodeFloat(OUTPUT_NODE, resu)
 
         //inference.run(..)
-        this.inference!!.fetch("Tanh_1", this.raw)
+        this.inference.fetch("Tanh_1", this.raw)
 
-        for (i in 0..intValues.size - 1) {
+        val pixelsInBitmap = manipulatePixelsInBitmap()
+        val bitmap = manipulateBitmap(width, height, pixelsInBitmap)
+        return bitmap
+    }
 
-            val raw_one:Int = (((raw[i * 3]+1)/2.0 * 255).toInt()) shl 16
-            val raw_two:Int = (((raw[i * 3 + 1]+1)/2.0 * 255).toInt()) shl 8
-            val raw_three:Int = ((raw[i * 3 + 2]+1)/2.0 * 255).toInt()
-            intValues[i] = 0xFF000000.toInt() or raw_one or raw_two or raw_three
+    private fun manipulatePixelsInBitmap(): IntArray {
+        val pixelsInBitmap = IntArray(width * height)
+        for (i in 0..pixelsInBitmap.size - 1) {
+
+            val raw_one: Int = (((raw[i * 3] + 1) / 2.0 * 255).toInt()) shl 16
+            val raw_two: Int = (((raw[i * 3 + 1] + 1) / 2.0 * 255).toInt()) shl 8
+            val raw_three: Int = ((raw[i * 3 + 2] + 1) / 2.0 * 255).toInt()
+            pixelsInBitmap[i] = 0xFF000000.toInt() or raw_one or raw_two or raw_three
         }
-        bitmap!!.setPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        return pixelsInBitmap
+    }
+
+    private fun manipulateBitmap(width: Int, height: Int, pixelsInBitmap: IntArray): Bitmap {
+        val bitmap: Bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        bitmap.setPixels(pixelsInBitmap, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
         return bitmap
     }
 
