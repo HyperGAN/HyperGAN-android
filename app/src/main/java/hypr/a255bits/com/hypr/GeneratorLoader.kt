44b664c5f001ec84dc75e97d5826b809af8f3061
Mikkel Garcia <mikkel@255bits.com>
2017-08-01 00:37:48 -0600

    [feature] encode bitmap and re-decode

--- app/src/main/java/hypr/a255bits/com/hypr/GeneratorLoader.kt
+++ app/src/main/java/hypr/a255bits/com/hypr/GeneratorLoader.kt
@@ -10,6 +10,7 @@ class GeneratorLoader {
     var channels = 3
     var width = 128
     var height = 128
+    var z_dims = 1 * 16 * 16 * 96
 
     var raw: FloatArray = FloatArray(width * height * channels)
 
@@ -22,8 +23,14 @@ class GeneratorLoader {
 
     }
 
-    fun sample(): Bitmap {
+    fun sample(z:FloatArray): Bitmap {
         print("Sampling ")
+
+
+        var dims = longArrayOf(1.toLong(), 16.toLong(), 16.toLong(), 96.toLong())
+        this.inference.feed(
+
+                "concat", z, *dims);
 
         this.inference.run(arrayOf("Tanh_1"))
         //inference.readNodeFloat(OUTPUT_NODE, resu)
@@ -35,6 +42,31 @@ class GeneratorLoader {
         val bitmap = manipulateBitmap(width, height, pixelsInBitmap)
         return bitmap
     }
+
+    fun encode(bitmap: Bitmap): FloatArray {
+        val intValues = IntArray(width * height)
+        val floatValues = FloatArray(width * height * channels)
+        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        for (i in 0..intValues.size - 1) {
+            val ival = intValues[i]
+            floatValues[i * 3] = (ival shr 16 and 0xFF) / 255.0f
+            floatValues[i * 3 + 1] = (ival shr 8 and 0xFF) / 255.0f
+            floatValues[i * 3 + 2] = (ival and 0xFF) / 255.0f
+        }
+        var dims = longArrayOf(1.toLong(), width.toLong(), height.toLong(), channels.toLong())
+        this.inference.feed(
+
+                "input", floatValues, *dims);
+
+        this.inference.run(arrayOf("Tanh"))
+
+        var z: FloatArray = FloatArray(z_dims)
+
+        this.inference.fetch("Tanh", z)
+
+        return z
+    }
+
 
     private fun manipulatePixelsInBitmap(): IntArray {
         val pixelsInBitmap = IntArray(width * height)
