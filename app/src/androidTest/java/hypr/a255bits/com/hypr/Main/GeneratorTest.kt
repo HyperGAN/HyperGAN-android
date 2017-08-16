fa36fc9ab35f1997ba40bd2f3ffba0be53635e79
ted <tedhoryczuniv@outlook.com>
2017-08-16 12:33:23 -0400

    Save multiple images to android gallery

--- app/src/androidTest/java/hypr/a255bits/com/hypr/Main/GeneratorTest.kt
+++ app/src/androidTest/java/hypr/a255bits/com/hypr/Main/GeneratorTest.kt
@@ -1,12 +1,10 @@
 package hypr.a255bits.com.hypr.Main
 
 
-import android.app.Activity
 import android.content.Context
 import android.content.res.AssetManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.provider.MediaStore
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.test.suitebuilder.annotation.LargeTest
@@ -17,32 +15,51 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.BufferedInputStream
+import java.io.InputStream
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 open class GeneratorTest {
 
     @get:Rule open val mActivityTestRule = ActivityTestRule(MainActivity::class.java)
-    val generator = GeneratorLoader()
+    private val generator = GeneratorLoader()
+    private val IMAGES_TO_TRANSFER_FOLDERNAME = "beforeImages"
 
     @Test
     fun generatorTest() {
-        val imageFile = mActivityTestRule.activity.applicationContext.resources.assets.open("images/image.jpg")
-        val imageReaderFromFile = BufferedInputStream(imageFile)
-        val image = BitmapFactory.decodeStream(imageReaderFromFile)
+        mActivityTestRule.activity.requestPermissions(arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+        val listOfImages = mActivityTestRule.activity.applicationContext.resources.assets.list(IMAGES_TO_TRANSFER_FOLDERNAME)
+        writeTransformedBitmapsToAndroidGallery(listOfImages)
+    }
 
+    private fun writeTransformedBitmapsToAndroidGallery(listOfImages: Array<out String>?) {
+        listOfImages?.forEach { filename ->
+            val imageFile = openFile("$IMAGES_TO_TRANSFER_FOLDERNAME/$filename")
+            val image = getBitmapFromFile(imageFile)
+            val encodedBitmap = encodeImage(mActivityTestRule.activity.assets, image)
+            writeImageToFileOnAndroidDeviceGallery(encodedBitmap, mActivityTestRule.activity.applicationContext)
+        }
+    }
+
+    private fun openFile(filenamePath: String): InputStream? {
+        return mActivityTestRule.activity.applicationContext.resources.assets.open(filenamePath)
+    }
+
+
+    private fun getBitmapFromFile(imageFile: InputStream?): Bitmap? {
+        val imageReaderFromFile = BufferedInputStream(imageFile)
+        return BitmapFactory.decodeStream(imageReaderFromFile)
+    }
+
+    private fun encodeImage(assets: AssetManager?, image: Bitmap?): Bitmap {
         generator.load(mActivityTestRule.activity.assets)
         val scaled = Bitmap.createScaledBitmap(image, 128, 128, false)
         val encoded = generator.encode(scaled)
-        val encodedBitmap = generator.sample(encoded)
-        mActivityTestRule.activity.requestPermissions(arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
-
-        writeBitmapToFile(encodedBitmap, mActivityTestRule.activity.applicationContext)
+        return generator.sample(encoded)
     }
 
-    private fun writeBitmapToFile(encodedBitmap: Bitmap, context: Context) {
+    private fun writeImageToFileOnAndroidDeviceGallery(encodedBitmap: Bitmap, context: Context) {
         ImageSaver().saveImageToInternalStorage(encodedBitmap, context, "after")
-        Thread.sleep(1000)
     }
 
 }
