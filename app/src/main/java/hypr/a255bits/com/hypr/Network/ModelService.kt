9faf6f30361cef062638101364b8fbfc522f8208
ted <tedhoryczuniv@outlook.com>
2017-08-17 13:04:03 -0400

    Added in app billing to JSON POJO

--- app/src/main/java/hypr/a255bits/com/hypr/Network/ModelService.kt
+++ app/src/main/java/hypr/a255bits/com/hypr/Network/ModelService.kt
@@ -1,13 +1,12 @@
 package hypr.a255bits.com.hypr.Network
 
 import hypr.a255bits.com.hypr.Generator.Generator
-import hypr.a255bits.com.hypr.Generator.Generator_
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface ModelService{
 
-    @GET("https://gist.githubusercontent.com/TedHoryczun/787f524fd2bdd260797cf6c8be854e05/raw/7b34a1f0a4a4e8ad86a68306a616a83d78608a2b/dummy_hypr.json")
+    @GET("https://gist.githubusercontent.com/TedHoryczun/787f524fd2bdd260797cf6c8be854e05/raw/a4a026d98b55172a9a3db4bddcfb54ad9ab72431/dummy_hypr.json")
     fun listOfModels(): Call<List<Generator>>?
 }
 
