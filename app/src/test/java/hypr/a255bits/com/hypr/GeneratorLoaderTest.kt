448070b3006ecde00a58e950434ad89109f31f85
ted <tedhoryczuniv@outlook.com>
2017-08-17 12:48:45 -0400

    init robolectric code

--- app/src/test/java/hypr/a255bits/com/hypr/GeneratorLoaderTest.kt
+++ app/src/test/java/hypr/a255bits/com/hypr/GeneratorLoaderTest.kt
@@ -1,26 +1,61 @@
 package hypr.a255bits.com.hypr
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.support.v4.graphics.BitmapCompat
-import android.test.ActivityTestCase
+import hypr.a255bits.com.hypr.Main.MainActivity
+import hypr.a255bits.com.hypr.Util.ImageSaver
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import java.io.BufferedInputStream
 import java.io.File
-import junit.framework.Assert
-import java.io.BufferedReader
-import java.io.FileReader
-import java.io.IOException
+import java.io.InputStream
+
+@Config(manifest = "/src/main/AndroidManifest.xml")
+@RunWith(RobolectricTestRunner::class)
+open class GeneratorLoaderTest {
+    private val IMAGES_TO_TRANSFER_FOLDERNAME = "beforeImages"
+    var activity: MainActivity? = null
+    private val generator = GeneratorLoader()
+
+    @Test
+    fun generatorTest() {
+        activity = Robolectric.setupActivity(MainActivity::class.java)
+        val beforeImagesFolder = this.javaClass.classLoader.getResource(IMAGES_TO_TRANSFER_FOLDERNAME).file
+        val listOfFiles = File(beforeImagesFolder).listFiles()
+        writeTransformedBitmapsToAndroidGallery(listOfFiles)
+    }
+
+    private fun writeTransformedBitmapsToAndroidGallery(listOfImages: Array<File>) {
+        listOfImages.forEach { file ->
+            val image = getBitmapFromFile(file.inputStream())
+            val encodedBitmap = encodeImage(image)
+            writeImageToFileOnAndroidDeviceGallery(encodedBitmap, activity!!.applicationContext)
+        }
+    }
+
+    private fun openFile(filenamePath: String): InputStream? {
+        return activity!!.applicationContext.resources.assets.open(filenamePath)
+    }
 
 
-class GeneratorLoaderTest {
-    val generatorLoader = GeneratorLoader()
+    private fun getBitmapFromFile(imageFile: InputStream?): Bitmap? {
+        val imageReaderFromFile = BufferedInputStream(imageFile)
+        return BitmapFactory.decodeStream(imageReaderFromFile)
+    }
 
+    private fun encodeImage(image: Bitmap?): Bitmap {
+        generator.load(activity!!.assets)
+        val scaled = Bitmap.createScaledBitmap(image, 128, 128, false)
+        val encoded = generator.encode(scaled)
+        return generator.sample(encoded)
+    }
 
-    fun load_and_feed() {
-//        generatorLoader.load(activity.assets)
-//
-//        generatorLoader.sample(encoded)
-//        assertNotNull(generatorLoader)
+    private fun writeImageToFileOnAndroidDeviceGallery(encodedBitmap: Bitmap, context: Context) {
+        ImageSaver().saveImageToInternalStorage(encodedBitmap, context, "after")
     }
 
     @Test
