e9896dbf7503ae21481e38cd7a1320d7a0a78fe8
tedhoryczun <tedhoryczuniv@outlook.com>
2017-09-08 12:15:44 -0400

    deleted tag

--- app/src/main/java/hypr/a255bits/com/hypr/Util/AnalyticsEvent.kt
+++ app/src/main/java/hypr/a255bits/com/hypr/Util/AnalyticsEvent.kt
@@ -1,8 +1,5 @@
 package hypr.a255bits.com.hypr.Util
 
-/**
- * Created by tedho on 9/7/2017.
- */
 enum class AnalyticsEvent{
     GENERATOR_DOWNLOAD, EXISTING_PHOTO, TAKE_PICTURE, SHARE_IMAGE, SAVE_IMAGE, CHOOSE_SIDE_NAV_OPTION, CHOOSE_HOME_NAV_OPTION
 }
