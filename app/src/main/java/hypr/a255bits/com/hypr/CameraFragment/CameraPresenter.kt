9ce3e5b9e4bf9763516cc498f58452471a400715
tedhoryczun <tedhoryczuniv@outlook.com>
2017-10-13 10:47:01 -0400

    No face detected camera toast popup

--- app/src/main/java/hypr/a255bits/com/hypr/CameraFragment/CameraPresenter.kt
+++ app/src/main/java/hypr/a255bits/com/hypr/CameraFragment/CameraPresenter.kt
@@ -1,23 +1,39 @@
 package hypr.a255bits.com.hypr.CameraFragment
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import hypr.a255bits.com.hypr.Util.Analytics
 import hypr.a255bits.com.hypr.Util.AnalyticsEvent
+import hypr.a255bits.com.hypr.Util.FaceDetection
 import hypr.a255bits.com.hypr.Util.ImageSaver
+import org.jetbrains.anko.longToast
 
 
 class CameraPresenter(val view: CameraMVP.view, val context: Context) : CameraMVP.presenter {
 
     val interactor: CameraInteractor by lazy{CameraInteractor(context)}
     val analytics = Analytics(context)
+    val faceDetection = FaceDetection(context)
     override fun sendPictureToModel(jpeg: ByteArray?) {
-        view.sendImageToModel(jpeg)
+        val bitmap = BitmapFactory.decodeByteArray(jpeg, 0, jpeg!!.size)
+        startModelIfImageHasFace(bitmap, jpeg)
+
+    }
+
+    private fun startModelIfImageHasFace(bitmap: Bitmap?, jpeg: ByteArray) {
+        if(bitmap?.let { faceDetection.getFaceLocations(it, context)?.get(0) } != null){
+            view.sendImageToModel(jpeg)
+        }else{
+            context.longToast("Please select an image with a face")
+        }
     }
 
     override fun getImageFromImageFileLocation(imageLocation: Uri) {
         val imageFromGallery: ByteArray? = ImageSaver().uriToByteArray(imageLocation, context)
-        view.sendImageToModel(imageFromGallery)
+        val bitmap = BitmapFactory.decodeByteArray(imageFromGallery, 0, imageFromGallery!!.size)
+        startModelIfImageHasFace(bitmap, imageFromGallery)
     }
 
     fun captureImage() {
