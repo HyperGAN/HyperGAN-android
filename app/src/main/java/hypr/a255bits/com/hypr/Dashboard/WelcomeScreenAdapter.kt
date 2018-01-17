d9f63dd6ea46e102ef76b1af74cd6f0a11acf138
tedhoryczun <tedhoryczuniv@outlook.com>
2018-01-17 13:27:13 -0500

    Images for each model

--- app/src/main/java/hypr/a255bits/com/hypr/Dashboard/WelcomeScreenAdapter.kt
+++ app/src/main/java/hypr/a255bits/com/hypr/Dashboard/WelcomeScreenAdapter.kt
@@ -1,12 +1,14 @@
 package hypr.a255bits.com.hypr.Dashboard
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
 import com.pawegio.kandroid.alert
@@ -16,7 +18,7 @@ import kotlinx.android.synthetic.main.model_card.view.*
 import org.greenrobot.eventbus.EventBus
 
 
-class WelcomeScreenAdapter(val generators: MutableList<BuyGenerator>, val context: Context) : RecyclerView.Adapter<WelcomeScreenAdapter.CustomViewHolder>() {
+class WelcomeScreenAdapter(val generators: MutableList<BuyGenerator>, val context: Context, val generatorImages: List<Bitmap>) : RecyclerView.Adapter<WelcomeScreenAdapter.CustomViewHolder>() {
 
     override fun getItemCount(): Int {
         return generators.size
@@ -32,6 +34,7 @@ class WelcomeScreenAdapter(val generators: MutableList<BuyGenerator>, val contex
         holder?.title?.text = generators[position].name
         buyButtonClickListener(holder, position)
         holder?.card?.setOnClickListener {EventBus.getDefault().post(position.toDouble()) }
+        holder?.cardImage?.setImageBitmap(generatorImages[position])
     }
 
     private fun buyButtonClickListener(holder: CustomViewHolder?, position: Int) {
@@ -54,6 +57,7 @@ class WelcomeScreenAdapter(val generators: MutableList<BuyGenerator>, val contex
         val buyButton: Button = view.buyButton
         val card: CardView = view.card
         val background: RelativeLayout = view.backgroundLayout
+        val cardImage: ImageView = view.cardImage
 
     }
 }
