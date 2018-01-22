df64eb0d5dcea9b79c61bc3b83d571a0c747de25
tedhoryczun <tedhoryczuniv@outlook.com>
2018-01-22 10:56:13 -0500

    Added feature class for adding and removing features

--- app/src/main/java/hypr/a255bits/com/hypr/GeneratorLoader/EasyGeneratorLoader.kt
+++ app/src/main/java/hypr/a255bits/com/hypr/GeneratorLoader/EasyGeneratorLoader.kt
@@ -12,7 +12,7 @@ class EasyGeneratorLoader(var gen: Generator) : GeneratorLoader() {
     var baseImage: Bitmap? = null
     var encoded: FloatArray? = null
     var mask: FloatArray by Delegates.vetoable(floatArrayOf()) { property, oldValue, newValue ->
-        featureEnabled("mask")
+        featureEnabled(Feature.MASK)
     }
     var direction: FloatArray? = null
     val inliner = InlineImage()
@@ -26,7 +26,7 @@ class EasyGeneratorLoader(var gen: Generator) : GeneratorLoader() {
         val scaled = Bitmap.createScaledBitmap(image, generator?.generator?.output?.width!!, generator?.generator?.output?.height!!, false)
         baseImage = scaled
         mask = this.mask(scaled)
-        val image = if (featureEnabled("encoding")) {
+        val image = if (featureEnabled(Feature.ENCODING)) {
             encoded = this.encode(scaled)
             this.sample(encoded!!, 0.0f, mask, direction!!, scaled).toBitmap(this.width, this.height)
         } else {
@@ -40,7 +40,7 @@ class EasyGeneratorLoader(var gen: Generator) : GeneratorLoader() {
         mask = this.mask(scaled)
         val direction = this.random_z()
         baseImage = scaled
-        val sample = if(featureEnabled("encoding")){
+        val sample = if(featureEnabled(Feature.ENCODING)){
             encoded = this.encode(scaled)
             this.sampleRandom(encoded!!, 0.0f, direction, mask, scaled)
 
@@ -57,7 +57,7 @@ class EasyGeneratorLoader(var gen: Generator) : GeneratorLoader() {
 
     fun inlineImage(person: Person, newCroppedImage: Bitmap, croppedPoint: Rect): Bitmap? {
         val image: Bitmap?
-        if (featureEnabled("inline")) {
+        if (featureEnabled(Feature.INLINE)) {
             val fullImage = person.fullImage
             val faceImage = person.faceImage?.toBitmap()
             image = if (faceImage != null) {
