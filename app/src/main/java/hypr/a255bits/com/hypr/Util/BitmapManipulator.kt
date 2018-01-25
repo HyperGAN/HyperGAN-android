9aab1ac7f40cd4d2b9d5c6b5fc5aa9e4e67c5efe
tedhoryczun <tedhoryczuniv@outlook.com>
2018-01-25 14:04:27 -0500

    clean up getting bitmap shape

--- app/src/main/java/hypr/a255bits/com/hypr/Util/BitmapManipulator.kt
+++ app/src/main/java/hypr/a255bits/com/hypr/Util/BitmapManipulator.kt
@@ -4,9 +4,19 @@ import android.graphics.Bitmap
 
 
 class BitmapManipulator{
+    fun determineBitmapShape(width: Int, height: Int): BitmapShape {
+        var shape = BitmapShape.Square
+        if(width > height){
+           shape = BitmapShape.Landscape
+        }else if( height > width){shape = BitmapShape.Portrate}
+        return shape
+    }
 
     fun cropAreaOutOfBitmap(image: Bitmap, x: Int, y: Int, width: Int, height: Int): Bitmap {
         return Bitmap.createBitmap(image, x, y, width, height)
     }
 
+}
+enum class BitmapShape{
+    Square, Landscape, Portrate
 }
